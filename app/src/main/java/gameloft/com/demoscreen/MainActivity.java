b48a8cfb7b47a5a534211ad6b043007270d3c712
Trương Quang Sơn <sontruongquangson@gmail.com>
2018-09-16 20:48:07 +0700

    first commit

---
+++ app/src/main/java/gameloft/com/demoscreen/MainActivity.java
@@ -0,0 +1,13 @@
+package gameloft.com.demoscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
